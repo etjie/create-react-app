0bd9ca8f0b0e29ca9bc3852cf6f150464ea07981
etjie <syafrullah.djaya@gmail.com>
2020-03-28 00:16:59 +0700

    test ci lagi

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World.</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is amazing..</p>
         <a
           className="App-link"
           href="https://reactjs.org"
